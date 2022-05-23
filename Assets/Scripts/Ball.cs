fa9775c5d947848243f5df3e69db983f01204754
Tom <19222781+tomling@users.noreply.github.com>
2022-05-23 17:16:14 +0100

    Add physics to ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rigidbody;
+
+    public float _speed = 200.0f;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-
+        AddStartingForce();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddStartingForce()
     {
+        var x = Random.value < 0.5f ? -1.0f : 1.0f;
+        var y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1f);
 
+        var Direction = new Vector2(x, y);
+
+        _rigidbody.AddForce(Direction * _speed);
     }
+
 }
